f1f7c002222659d74d8422397b0a69a883940747
stlp <22071107+gundry2@users.noreply.github.com>
2024-02-01 08:51:45 -0800

    Small update to valid_sudoku to optimize; 99% runtime, 77% memory

--- arrays_hashing/valid_sudoku.py
+++ arrays_hashing/valid_sudoku.py
@@ -21,29 +21,29 @@ class Solution:
                 colnum = j
                 # there is a way to do this with math that I'm too
                 # tired to think of right now
-                if i < 3:
-                    if j < 3:
-                        gridnum = 0
-                    elif j < 6:
-                        gridnum = 1
-                    else:
-                        gridnum = 2
-                elif i < 6:
-                    if j < 3:
-                        gridnum = 3
-                    elif j < 6:
-                        gridnum = 4
-                    else:
-                        gridnum = 5
-                else:
-                    if j < 3:
-                        gridnum = 6
-                    elif j < 6:
-                        gridnum = 7
-                    else:
-                        gridnum = 8
+                gridnum = (3 * (i // 3)) + j // 3
+                # if i < 3:
+                #     if j < 3:
+                #         gridnum = 0
+                #     elif j < 6:
+                #         gridnum = 1
+                #     else:
+                #         gridnum = 2
+                # elif i < 6:
+                #     if j < 3:
+                #         gridnum = 3
+                #     elif j < 6:
+                #         gridnum = 4
+                #     else:
+                #         gridnum = 5
+                # else:
+                #     if j < 3:
+                #         gridnum = 6
+                #     elif j < 6:
+                #         gridnum = 7
+                #     else:
+                #         gridnum = 8
                 
-                # gridnum = (i // 3) + (j // 3) # hmm. I think this is it?
                 if num in sets[rownum][0] or num in sets[colnum][1] \
                     or num in sets[gridnum][2]:
                     return False
