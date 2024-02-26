3c16bad3a5766461673489177a95965ae1e03111
stlp <22071107+gundry2@users.noreply.github.com>
2024-02-26 09:05:21 -0800

    koko eating bananas. no idea why it works, had to use solution

--- binary_search/koko_eating_bananas.py
+++ binary_search/koko_eating_bananas.py
@@ -1,48 +1,31 @@
+import math 
 from typing import List
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        """Super brute force because I cannot figure out how this
-        relates to binary search?"""
-        n = len(piles)
-
-        if n > h:
-            # Cannot eat them all in time.
-            return False
-        if n == h:
-            return max(piles)
-
-        # Binary search between ceil(sum(piles) / h ) and max(piles)???
-        total = sum(piles)
-        left = total // h + 1
+        left = 1
         right = max(piles)
 
-        # Maybe could DP and build out a time matrix...hm
-        while left <= right:
+        while left < right:
             mid = (left + right) // 2
-            curr = self.verify_pile_time(piles, mid)
-            if curr == h:
-                # left = right + 1
-                break
-                # return curr
-            elif curr < h:
-                # need to eat it slower
-                left = mid + 1
+            curr = self.timeToEat(piles, mid)
+            if curr <= h:
+                right = mid
             else:
-                # need to eat it faster
-                right = mid - 1
+                left = mid + 1
+                
+        return left
 
-        mid = (left + right) // 2
-        curr = self.verify_pile_time(piles, mid)
-        return curr
-
-    def verify_pile_time(self, piles, k):
-        time = 0
+    def timeToEat(self, piles, k):
+        return sum(math.ceil(pile / k) for pile in piles)
+        total = 0
         for pile in piles:
-            time += -(pile // -k)
+            total += math.ceil(pile / k)
 
-        return time
-        
+        return total
+
+
+                
 if __name__ == "__main__":
     s = Solution()
     print(s.minEatingSpeed([3,6,7,11], 8))
