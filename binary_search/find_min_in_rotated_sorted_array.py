b70bf8f6eb000c4d87897a74282665633f6f5689
stlp <22071107+gundry2@users.noreply.github.com>
2024-02-27 09:19:48 -0800

    find minimum in rotated sorted array in O(n) time, O(1) space

--- binary_search/find_min_in_rotated_sorted_array.py
+++ binary_search/find_min_in_rotated_sorted_array.py
@@ -2,18 +2,29 @@ from typing import List
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        # Can optimize by combining the two loops below,
-        # but for now separate them.
         n = len(nums)
-
-        # First, find where the array was rotated to.
+        if n == 1:
+            return nums[0]
         left = 0
         right = n - 1
         while left <= right:
             mid = (left + right) // 2
-            if nums[mid] > nums[(mid - 1) % n] and nums[mid] < nums[(mid + 1) % n]:
-                # This is in sorted order.
-                # god idk. I don't have time to write this out today :\
+            if nums[(mid - 1) % n] < nums[mid] and nums[mid] > nums[(mid + 1) % n]:
+                # Found the end of the original array.
+                left = right + 1
+            elif nums[left] > nums[mid]:
+                # must be in the left half, since at some point in left
+                # half it must switch from increasing to decreasing.
+                right = mid - 1
+            else:
+                left = mid + 1
 
-    def binSearch(self, nums):
+        # We know the start is the smallest since it's in sorted order,
+        # so just return that.
+        return nums[(mid + 1) % n]
         
+if __name__ == "__main__":
+    s = Solution()
+    print(s.findMin([3,4,5,1,2]))
+    print(s.findMin([4,5,6,7,0,1,2]))
+    print(s.findMin([11,13,15,17]))
