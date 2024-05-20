1aa8dbf4a2bc8eca96ac9a000b0c78de1bc9dc5d
stlp <22071107+gundry2@users.noreply.github.com>
2024-05-20 11:55:10 -0700

    Brute force approach to reorder_linked_list

---
+++ linked_list/reorder_linked_list.py
@@ -0,0 +1,58 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        n = 0
+        curr = head
+        while curr != None:
+            n += 1
+            curr = curr.next
+
+        # Split the lists into halves.
+        prev = None
+        mid = head
+        mid_count = 0
+        while mid_count < n / 2:
+            prev = mid
+            mid = mid.next
+            mid_count += 1
+
+        curr = head 
+        prev.next = None
+        print(f"mid: {mid}")
+
+        # Reverse the right half of the list...
+        prev = None
+        while mid != None:
+            # print(f"nxt: {mid.next}")
+            nxt = mid.next
+            mid.next = prev
+            prev = mid
+            mid = nxt
+            # print(f"prev: {prev}")
+
+        # print(head)
+        # print(prev)
+
+        # Then interlink the nodes together.
+        while curr != None and prev != None:
+            temp_left = curr.next
+            temp_right = prev.next
+            curr.next = prev
+            prev.next = temp_left
+            curr = temp_left
+            prev = temp_right
+
+        return head
+
+        
