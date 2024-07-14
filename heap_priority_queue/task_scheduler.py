7924a92ae3da2079b097bef51339c3ed266b7fd2
stlp <22071107+gundry2@users.noreply.github.com>
2024-07-14 14:09:54 -0400

    extremely suboptimal task scheduler

--- heap_priority_queue/task_scheduler.py
+++ heap_priority_queue/task_scheduler.py
@@ -1,17 +1,39 @@
 import heapq
-from collections import Counter
+import collections
 from typing import List
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        """priority queue"""
-        count = Counter(tasks)
-        q = []
-        time = 0
-        while count:
-            time += 1
-            count.most_common(1)[0]
+        intervals = 0
+        heap = []
+        count = collections.Counter(tasks)
+        # print(count)
+        for key, cnt in count.items():
+            heapq.heappush(heap, [0, -cnt, key])
 
+        while heap:
+            # add cooldown time of next item if there is one
+            if heap[0][0] > 0:
+                x = heap[0][0]
+                intervals += x
+                for i in heap:
+                    i[0] -= x
+                # print(f"heap after reducing interval: {heap}")
+                # print(f"intervals after reduction: {intervals}\n")
+
+            # pop it and update what's left
+            cooldown, count, key = heapq.heappop(heap)
+            for i in heap:
+                if i[2] != key:
+                    i[0] -= 1
+            intervals += 1
+            if count < -1:
+                heapq.heappush(heap, [cooldown + n, count + 1, key])
+            # print(f"intervals: {intervals}")
+            # print(f"heap now: {heap}\n\n")
+
+        return intervals
+        
 
 
 if __name__ == "__main__":
