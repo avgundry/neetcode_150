ce976be89c0f3860029985860b649aaa010eeb7e
stlp <22071107+gundry2@users.noreply.github.com>
2024-07-30 08:13:28 -0400

    subsets_ii update to include backtracking method.

--- backtracking/subsets_ii.py
+++ backtracking/subsets_ii.py
@@ -2,6 +2,31 @@ from typing import List
 
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        """Backtracking method."""
+        res = []
+        nums.sort()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                # Append a copy of the completed subset.
+                res.append(subset[::])
+                return
+
+            # Create all subsets including nums[i]
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+            
+            # Create all subsets NOT including nums[i]
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
+
+        return res
+
+        """Loop method."""
         nums.sort()
         subs = [[]]
 
