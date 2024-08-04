5dda18419da43d8a5a65ee434f02799576705637
stlp <22071107+gundry2@users.noreply.github.com>
2024-08-04 08:31:08 -0400

    Brute force product of array except self

--- arrays_hashing/product_of_array_except_self.py
+++ arrays_hashing/product_of_array_except_self.py
@@ -2,25 +2,62 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
+        """Brute force looping"""
+        pref_left = nums[::]
+        pref_right = nums[::]
+        
 
-        if n <= 2:
-            return [nums[1], nums[0]]
 
-        ret = [1 for _ in range(n)]
+        # On the first pass, we set up pref_left so that
+        # pref_left[i] is the product of all numbers to the left of nums[i].
+        for i in range(len(nums)):
+            if i == 0:
+                pref_left[i] = 1
+            else:
+                pref_left[i] = nums[i - 1] * pref_left[i - 1]
+        
+        # On the second pass we set up pref_right so that pref_right[i] is the
+        # product of all numbers to the right of nums[i].
+        for i in range(len(nums)):
+            if i != 0:
+                pref_right[-(i + 1)] = nums[-i] * pref_right[-i]
+            else:
+                pref_right[-(i + 1)] = 1
+        
+        # Finally, loop over those and multiply each together to get the final answer.
+        pref_final = nums[::]
+        for i in range(len(nums)):
+            pref_final[i] = pref_left[i] * pref_right[i]
+ 
 
-        ret[1] = nums[0]
-        # technically constant space? or...no. hm.
-        for i in range(2, n):
-            ret[i] = ret[i - 1] * nums[i - 1]
+        return pref_final
 
-        post = 1
-        for i in range(n - 1, -1, -1):
-            ret[i] *= post
-            post *= nums[i]
 
-        return ret
+        """Unfinished cleaner approach"""
+        pref = nums[::]
 
+
+        # Simultaneously go from left to right, meet in the middle
+        # the middle number wil be the product of left and right if even
+        # else the middle left number is prod of all #s to left
+        # middle right number is prod of all #s to right of it
+        # then you can go from there, maybe? 
+
+        # Still need to do two loops.
+        # for i in range(len(nums) // 2):
+        #     if i == 0:
+        #         pref[i] = 1
+        #         pref[-(i + 1)] = 1
+        #     else:
+        #         pref[i] = nums[i - 1] * pref[i - 1]
+        #         pref[-(i + 1)] = nums[-i] * pref[-i]
+
+        # pref[i + 1] = nums[i] * pref[i] * nums[i + 2] * pref[i + 2]
+        # Now loop to 'combine' the two sides, starting from the middle.
+        for i in range(len(nums) // 2, -1):
+            pref[i] *= pref[i ]
+
+        
 if __name__ == "__main__":
     s = Solution()
     print(s.productExceptSelf([1,2,3,4]))
