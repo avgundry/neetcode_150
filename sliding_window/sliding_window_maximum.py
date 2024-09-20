e5bc15ab65b04c21d340db07814b44de7fbd9cb1
stlp <22071107+gundry2@users.noreply.github.com>
2024-09-20 09:52:03 -0700

    sliding window maximum with heap approach. O(nlogn) time, amortized.

--- sliding_window/sliding_window_maximum.py
+++ sliding_window/sliding_window_maximum.py
@@ -5,44 +5,45 @@ import heapq
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        """Deque solution"""
-        currmax = float('-inf')
-        dq = deque()
-        ret = []
-
-        for i in range(k):
-            currmax = max(currmax, nums[i])
-            dq.append(nums[i])
-
-        ret.append(currmax)
-
-
-        for i in range(k, len(nums)):
-            if dq.popleft() == currmax:
-                currmax = max(dq, default=float('-inf'))
-            currmax = max(currmax, nums[i])
-            dq.append(nums[i])
-            ret.append(currmax)
-
-        return ret
-
-
-
-
-        """Brute force solution: iterate over each window"""
         n = len(nums)
+        if n < k or k == 0:
+            return []
+        elif k == 1:
+            return nums
+
+        # A heap that lazily stores the current window.
+        kheap = []
+        # Counter to keep track of how many of each num are in the current window.
+        count = Counter()
+        # The array we return.
         out = []
 
-        for i in range(n - k + 1):
-            window = nums[i:i + k]
-            out.append(max(window))
+        for i in range(n):
+            curr = nums[i]
+            count[curr] += 1
+            heapq.heappush(kheap, -curr)
+            if i == k - 1:
+                out.append(-kheap[0])
+            if i <= k - 1:
+                continue
+            else:
+                prev = nums[i - k]
+            
+            # Slide the window by decrementing the count.
+            count[prev] -= 1
+            # Remove any elements in the heap that are no longer in the window.
+            while count[-kheap[0]] <= 0:
+                heapq.heappop(kheap)
+
+            # Then add the largest element to our output.
+            out.append(-kheap[0])
 
         return out
 
-        
-
 if __name__ == "__main__":
     s = Solution()
+    print(s.maxSlidingWindow([-7,-8,7,5,7,1,6,0], 4))
+    print(s.maxSlidingWindow([7, 2, 4], 2))
     print(s.maxSlidingWindow([1,-1], 1))
     print(s.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
 
