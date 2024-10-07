7c14ea742a590ed797e502fd45ae21da94edf320
stlp <22071107+gundry2@users.noreply.github.com>
2024-10-07 09:27:53 -0700

    subtree of another tree

--- trees/subtree_of_another_tree.py
+++ trees/subtree_of_another_tree.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -7,21 +8,22 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if root == None:
+        if root is None:
             return False
-        if self.isSame(root, subRoot):
-            return True
+        elif root.val == subRoot.val:
+            # Check if the ENTIRE subtree is the same.
+            if self.isSame(root.left, subRoot.left) and self.isSame(root.right, subRoot.right):
+                return True
 
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
     def isSame(self, root, subRoot):
-        if root == None and subRoot == None:
+        if root is None and subRoot is None:
             return True
-        if root == None or subRoot == None:
+        if root is None or subRoot is None or root.val != subRoot.val:
             return False
-        if root.val != subRoot.val:
-            return False
-        return self.isSame(root.left, subRoot.val) and self.isSame(root.right, subRoot.right)
-        
+        return self.isSame(root.left, subRoot.left) and \
+            self.isSame(root.right, subRoot.right)
