09973aeab7884e4b1a82066fccd3e2bc19fbb637
stlp <22071107+gundry2@users.noreply.github.com>
2024-10-16 08:19:46 -0700

    Slightly suboptimal hand of straights, done in ~2 mins

--- greedy/hand_of_straights.py
+++ greedy/hand_of_straights.py
@@ -4,54 +4,25 @@ from typing import List
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         n = len(hand)
-
-        # If it's not evenly divisible by the groupsize, we obviously can't rearrange it.
         if n % groupSize != 0:
             return False
 
-        """Dictionary approach."""
-        cards = collections.defaultdict(lambda: 0)
-        for card in hand:
-            cards[card] += 1
-        
-        smallest = sorted(list(set(hand)), key=lambda x: -x)
-
-        while cards:
-            curr = smallest[-1]
-            for i in range(curr, curr + groupSize):
-                if cards[i] == 0:
-                    return False
-                else:
-                    cards[i] -= 1
-                    if cards[i] == 0:
-                        del cards[i]
-                        smallest.pop()
+        count = collections.Counter(sorted(hand))
+        while count:
+            card = list(count.keys())[0]
+            if all([count[card + i] > 0 for i in range(groupSize)]):
+                for i in range(groupSize):
+                    count[card + i] -= 1
+                count = +count
+            else:
+                return False
 
         return True
 
-
-
-        """Brute force"""
-        # Greedily remove groups.
-        while cards:
-            curr = max(cards.elements())
-            cards[curr] -= 1
-            temp = [curr]
-            for _ in range(-2, -1 - groupSize, -1):
-                if cards.get(curr - 1):
-                    curr -= 1
-                    temp.append(curr)
-                    cards[curr] -= 1
-                    cards = +cards
-                else:
-                    return False
-            cards = +cards
-
-
-        return True
 
 
 if __name__ == "__main__":
     s = Solution()
     print(s.isNStraightHand([1,2,3,6,2,3,4,7,8], 3))
     print(s.isNStraightHand([1,2,3,4,5], 4))
+    print(s.isNStraightHand([1], 1))
